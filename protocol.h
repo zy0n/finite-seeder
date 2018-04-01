c4994f0ad35e023170bb65ecfdf98011ce31d1cd
Shawn Barto <zy0n.barto@gmail.com>
2018-04-01 08:50:27 -0600

    one last port fix

--- protocol.h
+++ protocol.h
@@ -18,7 +18,7 @@
 extern bool fTestNet;
 static inline unsigned short GetDefaultPort(const bool testnet = fTestNet)
 {
-    return testnet ? 19999 : 9999;
+    return testnet ? 32223 : 23223;
 }
 
 //
