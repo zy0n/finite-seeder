f1a5854c48af71a144aa2beb4a7195aef0c299ad
Shawn Barto <zy0n.barto@gmail.com>
2018-04-01 09:09:18 -0600

    changed user-agent

--- bitcoin.cpp
+++ bitcoin.cpp
@@ -36,7 +36,7 @@ class CNode {
     nHeaderStart = vSend.size();
     vSend << CMessageHeader(pszCommand, 0);
     nMessageStart = vSend.size();
-   printf("%s: SEND %s\n", ToString(you).c_str(), pszCommand); 
+   printf("%s: SEND %s\n", ToString(you).c_str(), pszCommand);
   }
 
   void AbortMessage() {
@@ -80,7 +80,7 @@ class CNode {
     CAddress me(CService("0.0.0.0"));
     BeginMessage("version");
     int nBestHeight = GetRequireHeight();
-    string ver = "/darkcoin-seeder:0.1.2/";
+    string ver = "/Finite Core:0.12.2.3/";
     vSend << PROTOCOL_VERSION << nLocalServices << nTime << you << me << nLocalNonce << ver << nBestHeight;
     EndMessage();
   }
